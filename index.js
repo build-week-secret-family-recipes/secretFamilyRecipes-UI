d1797272aec31941cbf8ba8209b33a3b54fc81e4
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-31 12:43:07 -0400

    JS mouseover fix - THANK YOU PAT!

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 
 
-
-const mouseEvent = document.querySelectorAll("nav");
+const mouseEvent = document.querySelector("nav");
 mouseEvent.addEventListener("mouseover", function(event) {
-    event.target.style.color = "#a00000";
+    event.target.style.color = "#1f1f44";
 
     setTimeout(function() {
-        event.target.style.color = "#1f1f44";
+        event.target.style.color = "#e3e3e3";
     }, 900);
-
-    mouseEvent();
 });
 
 
 
-// Move Logo
-
-// TweenMax.to(moveLogo, 5, {x:179, y:1176, scale:2, skewX:45, rotation:360});
-
-
-// // Set Timeout then move logo back to start
-
-// setTimeout(() => {
-// TweenMax.to(moveLogo, 5, {x:46, y:4, scale:2, skewX:45, rotation:360});
-
-// }, 155000);
